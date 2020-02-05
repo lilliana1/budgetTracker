b76e23d678867fa2bc22c154398186216dcbd3fd
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-02-05 18:29:09 -0500

    added other files from old project

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(logger("dev"))
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes here
+// add compression 
+app.use(compression());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
